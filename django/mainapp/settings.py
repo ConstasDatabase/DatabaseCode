ded7b013c46458c0d7e7be9488023705ccdcbd93
Tanusri Harish <118092429+tanusrixh@users.noreply.github.com>
2024-03-01 18:39:48 -0500

    tfbc

--- django/mainapp/settings.py
+++ django/mainapp/settings.py
@@ -77,7 +77,9 @@ TEMPLATES = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://10.100.2.13:3000'
+    'http://10.100.2.13:3000',
+    'https://10.100.2.2:443',
+    'https://0.0.0.0:443'
 ]
 
 WSGI_APPLICATION = "mainapp.wsgi.application"
