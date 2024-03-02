993aa16b997ec4415990efe3fdd6dfcec7de5593
Tanusri Harish <118092429+tanusrixh@users.noreply.github.com>
2024-03-02 02:40:44 -0500

    fixing links

--- myapp/src/Axios.js
+++ myapp/src/Axios.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 
 export const api = axios.create({
-    baseURL: 'https://constaslab.chem.uwo.ca/'
+    baseURL: 'https://constaslab.chem.uwo.ca/api/'
 })
 
 
 export const getPost = async () => {
     try{
-        const response = await api.get('api/categories/');
+        const response = await api.get('categories/');
         return response.data;
     }catch (e) {
         console.error('Message: ', e);
