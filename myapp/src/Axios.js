f98ff6ab8fbe72e870df900ee5a79a7436f46e95
Tanusri Harish <118092429+tanusrixh@users.noreply.github.com>
2024-03-08 17:30:06 -0500

    tfbc

--- myapp/src/Axios.js
+++ myapp/src/Axios.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 
 export const api = axios.create({
-    baseURL: 'https://10.100.2.2/api/'
+    baseURL: 'http://10.100.2.13:7990/api/'
 })
 
 
 export const getPost = async () => {
     try{
-        const response = await api.get('categories/');
+        const response = await api.get('categories');
         return response.data;
     }catch (e) {
         console.error('Message: ', e);
