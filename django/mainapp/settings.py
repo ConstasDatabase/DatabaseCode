9d22f506d78b9e9e32c418f7e00fbbd771b2f78f
Tanusri Harish <118092429+tanusrixh@users.noreply.github.com>
2024-03-11 19:07:42 -0400

    tfbc

--- django/mainapp/settings.py
+++ django/mainapp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.environ.get("DB_SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://10.100.2.13', 'https://constaslab.chem.uwo.ca:443', 'https://constaslab.chem.uwo.ca', '10.100.2.13', 'constaslab.chem.uwo.ca', 'localhost']
+ALLOWED_HOSTS = ['http://10.100.2.13', 'https://constaslab.chem.uwo.ca:443', 'https://constaslab.chem.uwo.ca', '10.100.2.13', 'constaslab.chem.uwo.ca', 'localhost', 'https://localhost:443']
 
 
 # Application definition
