66d82eaeddb71a4fbc29058fd1045c2831b0378d
Tanusri Harish <118092429+tanusrixh@users.noreply.github.com>
2024-03-12 09:05:15 -0400

    tfbc

--- myapp/src/Axios.js
+++ myapp/src/Axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const api = axios.create({
-    baseURL: 'https://constaslab.chem.uwo.ca:443/apis/'
+    baseURL: 'https://constaslab.chem.uwo.ca:443/api/'
 })
 
 
