13eed382545b251a6fcb4bb11a630011888cfc8b
Tanusri Harish <118092429+tanusrixh@users.noreply.github.com>
2024-03-12 12:46:03 -0400

    tfbc

--- myapp/src/Axios.js
+++ myapp/src/Axios.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 
 export const api = axios.create({
-    baseURL: 'https://constaslab.chem.uwo.ca:443/apis/'
+    baseURL: 'https://constaslab.chem.uwo.ca:443/api/'
 })
 
 
 export const getPost = async () => {
     try{
-        const response = await api.get('categories/');
+        const response = await api.get('api/categories/');
         return response.data;
     }catch (e) {
         console.error('Message: ', e);
